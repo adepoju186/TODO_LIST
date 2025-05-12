5662dd7d681c95c9a87435a96576390bc585d6b3
Adepoju David <dadepoju346@gmail.com>
2025-05-12 22:51:37 +0100

    Trying to fix bug

--- TODO_LIST/settings.py
+++ TODO_LIST/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY ='django-insecure-ujulyag=8zd!hhi!^d6mfn+znj431_df_mrp=p6zqef&y7&wx3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
